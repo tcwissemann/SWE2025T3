d2b2ed2f8fd8c33c1d4c41d10ca8a3b1d619840b
Thomas Wissemann <tcwissemann@gmail.com>
2025-02-01 02:30:19 -0600

    tiny fix

--- source/config/settings.py
+++ source/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-12lan%j++kzuch37+z@j_vz95g8bo4$+#h_8t781cqicr-1y)1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['134.122.122.129', 'localhost', '127.0.0.1']
 
