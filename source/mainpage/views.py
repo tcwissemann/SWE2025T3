3cc206b95c1887341c2dd2bb21b0b002dfaaa0af
Swappuri <168345681+Swappuri@users.noreply.github.com>
2025-02-15 12:02:46 -0600

    Update views.py

--- source/mainpage/views.py
+++ source/mainpage/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 def about_us(request):
-    return render(request, "main-page.html")
+    featured_product = Product.objects.first()
+    if featured_product:
+        featured_product_price = featured_product.price_in_dollars()
+    else:
+        featured_product_price = None
+    return render(request, "main-page.html", {"featured_product": featured_product, "featured_product_price": featured_product_price})
