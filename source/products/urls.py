730e2c5d3738ea7e260bb15cab54fd2c2385c30e
Thomas Wissemann <tcwissemann@gmail.com>
2025-02-14 20:06:22 -0600

    added boilerplate logic to track cart items in cookie

--- source/products/urls.py
+++ source/products/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.catalog, name='Shop'),
-    path('product/', views.product, name="Product")
+    path('product/', views.product, name="Product"),
+    path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.view_cart, name='view_cart'),
 ]
--- source/products/views.py
+++ source/products/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+import json
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,3 +8,64 @@ def catalog(request):
 
 def product(request):
     return render(request, "product-detail.html")
+
+# general logic for when dynamic product page is implemented
+def add_to_cart(request, item_id):
+    if request.method == 'POST':
+        cart = request.COOKIES.get('cart', '[]')
+        cart = json.loads(cart)
+
+        if not isinstance(cart, dict):
+            cart = {}
+
+
+        if str(item_id) in cart:
+            cart[str(item_id)]['quantity'] += 1
+        else:
+            item = Item.objects.get(id=item_id)
+            cart[str(item_id)] = {
+                'quantity': 1,
+                'name': item.name,
+                'price': str(item.price)
+            }
+
+        response = HttpResponse('Added to cart')
+        response.set_cookie('cart', json.dumps(cart), max_age=604800) # 1 week
+
+        return response
+
+def view_cart(request):
+    cart = request.COOKIES.get('cart', '[]')
+    cart_items = json.loads(cart)
+
+    items = Item.objects.filter(id__in=cart_items)
+
+    return render(request, 'cart.html', {'items': items})
+
+def clear_cart(request):
+    response = HttpResponse('Cart cleared')
+    response.delete_cookie('cart')
+    return response
+
+def remove_from_cart(request, item_id):
+    cart = json.loads(request.COOKIES.get('cart', '{}'))
+    if str(item_id) in cart:
+        del cart[str(item_id)]
+
+    response = HttpResponse('Item removed')
+    response.set_cookie('cart', json.dumps(cart), max_age=604800)
+    return response
+
+def update_quantity(request, item_id):
+    quantity = int(request.POST.get('quantity', 1))
+    cart = json.loads(request.COOKIES.get('cart', '{}'))
+    
+    if str(item_id) in cart:
+        if quantity > 0:
+            cart[str(item_id)]['quantity'] = quantity
+        else:
+            del cart[str(item_id)]
+
+    response = HttpResponse('Quantity updated')
+    response.set_cookie('cart', json.dumps(cart), max_age=604800)
+    return response
