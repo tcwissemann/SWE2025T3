2d9281ef3d6e0d909707d23884cf3d478a52f8eb
jocelyn vega <jvegapic@stedwards.edu>
2025-03-06 00:33:53 -0600

    added barebones unclaimed order table

--- source/management/views.py
+++ source/management/views.py
@@ -3,6 +3,16 @@ from orders.models import Order, OrderItem
 # Create your views here.
 
 def dashboard(request):
-    unclaimed_orders = Order.objects.all().filter(claim__isnull=True)
+    unclaimed_orders_objects = Order.objects.all().filter(claim__isnull=True).order_by('date')
     
-    return render(request, 'staff-dashboard.html', {"orders": unclaimed_orders})
+    orders = []
+    
+    for order in unclaimed_orders_objects:
+        order_info = {}
+        order_info['number'] = f"#{order.id:06d}"
+        order_info['id'] = order.id
+        order_info['date'] = order.date.strftime('%d/%m/%Y - %H:%M%p')
+        orders.append(order_info)
+        
+    
+    return render(request, 'staff-dashboard.html', {"orders":orders})
