442c0cf188d3501c15f587075ab8cec0cb4cd9b4
jocelyn vega <jvegapic@stedwards.edu>
2025-03-25 11:08:19 -0500

    reorganized staff section names

--- source/management/urls.py
+++ source/management/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.staff_profile, name='Staff Profile'),
-    path('claim-orders/', views.staff_dashboard, name='Unclaimed Orders'),
+    path('dashboard/', views.staff_dashboard, name='Dashboard'),
     path('order-detail/<int:order_id>/', views.order_detail, name='order_detail'),
     path('order-detail/<int:order_id>/update-status/', views.update_order_status, name='update_order_status'),
 ]
