b484bc68a3793c0c0a4bb9f911353afb63daa2d1
swappur <157229750+swappur@users.noreply.github.com>
2025-05-02 08:43:46 -0500

    Update urls.py

--- source/management/urls.py
+++ source/management/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('dashboard/', views.staff_dashboard, name='Dashboard'),
     path('order-detail/<int:order_id>/', views.order_detail, name='order_detail'),
     path('order-detail/<int:order_id>/update-status/', views.update_order_status, name='update_order_status'),
+    path('analytics/', views.staff_analytics, name='staff-analytics'),
+    path('data/', views.staff_data_json, name='staff-data-json'),  
 ]
