15be6d906acbc8f7e10faa158b55ace2f585e46d
Paloma Escobedo <pescobed@stedwards.edu>
2025-05-04 23:26:46 -0500

    contact functionality

--- source/contact/migrations/0001_initial.py
+++ source/contact/migrations/0001_initial.py
@@ -15,8 +15,6 @@ class Migration(migrations.Migration):
             name='ContactMessage',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('email', models.EmailField(max_length=254)),
                 ('subject', models.CharField(max_length=200)),
                 ('message', models.TextField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
