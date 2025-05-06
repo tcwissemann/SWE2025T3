043fb38eaf9244f860d3d938bf6d6e3f52cee29b
Paloma Escobedo <pescobed@stedwards.edu>
2025-05-05 19:04:51 -0500

    minor models fix

--- source/contact/models.py
+++ source/contact/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class ContactMessage(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField(max_length=254, null=True, blank=True)
     subject = models.CharField(max_length=200)
     message = models.TextField()
